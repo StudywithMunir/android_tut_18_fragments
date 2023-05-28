9c94627fa9c015aabf21689c0755b8b62e8abf31
MunirLovesCoding <studywithmunir18@gmail.com>
2023-05-28 12:59:16 +0500

    Final commit

---
+++ app/src/main/java/com/example/android_tut_18_fragments/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.android_tut_18_fragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onLoginClicked(View view) {
+        replaceFragment(new LoginFragment());
+    }
+
+    public void onRegisterClicked(View view) {
+        replaceFragment(new RegisterFragment());
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+}
---
+++ app/src/main/java/com/example/android_tut_18_fragments/RegisterFragment.java
@@ -0,0 +1,18 @@
+package com.example.android_tut_18_fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class RegisterFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_register, container, false);
+        return view;
+    }
+}
+
